7b0225724e0b37defa81e1903126120a8fdf4b62
Bjarte Aarmo Lund <bjarte.lund@uit.no>
2020-03-26 17:10:28 +0000

    Helper script to average two csv-files on the last column (averaging absorbances from two runs) using pandas

---
+++ csv-column-average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import pandas as pd
+import sys
+datafile1=sys.argv[1]
+datafile2=sys.argv[2]
+df1=pd.read_csv(datafile1)
+df2=pd.read_csv(datafile2)
+df1.iloc[:,-1]=(df1.iloc[:,3]+df2.iloc[:,3])/2
+df1.to_csv("averaged.csv",index=False)
+
