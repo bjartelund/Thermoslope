65178968e524e5e091b4426de086aa557d052fb5
Bjarte Aarmo Lund <bjarte.lund@uit.no>
2021-05-27 20:08:33 +0000

    Following https://gist.github.com/fperez/9716279 a jupyter-export-mode is under development, due to be merged within the Flask-app

---
+++ export-jupyter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import thermoslope
+import os
+import json
+from nbformat import v4 as nbf
+import nbformat
+
+nb =nbf.new_notebook()
+nbcells= []
+def analyze(analysisuuid):
+    if analysisuuid:
+        nbcells.append(nbf.new_markdown_cell(analysisuuid))
+        uploaddir = os.path.join("user-contrib/", analysisuuid)
+        if os.path.exists(uploaddir):
+            datafiles = [x for x in os.listdir(
+                uploaddir) if "png" not in x and "json" not in x]
+            fullpathdatafiles = [os.path.join(
+                "user-contrib", analysisuuid, datafile) for datafile in datafiles]
+            settings = json.load(open(os.path.join(uploaddir,"settings.json")))
+            nbcells.append(nbf.new_code_cell("import thermoslope"))
+            nbcells.append(nbf.new_code_cell("import os"))
+            nbcells.append(nbf.new_code_cell("import json"))
+            nbcells.append(nbf.new_code_cell("analysisuuid='%s'" % analysisuuid))
+            nbcells.append(nbf.new_code_cell("uploaddir = os.path.join('user-contrib/', analysisuuid)"))
+            nbcells.append(nbf.new_code_cell("settings = json.load(open(os.path.join(uploaddir,'settings.json')))"))
+
+            nbcells.append(nbf.new_code_cell("datafiles = [x for x in os.listdir(uploaddir) if 'png' not in x and 'json' not in x]"))
+            nbcells.append(nbf.new_code_cell("fullpathdatafiles = [os.path.join('user-contrib', analysisuuid, datafile) for datafile in datafiles]"))
+            nbcells.append(nbf.new_code_cell("analysis = thermoslope.ThermoSlope(fullpathdatafiles, **settings)"))
+            #nbcells.append(nbf.new_code_cell("""\
+            #        %pylab inline
+            #        print(**settings);"""))
+            
+            nbcells.append(nbf.new_code_cell("analysis.process()"))
+            analysis = thermoslope.ThermoSlope(fullpathdatafiles, **settings)
+            analysis.process()
+            return analysis
+result=analyze("a0ef33f01b1247a9a73603b2314df3c6")
+print(result)
+print(dir(result))
+nb["cells"]=nbcells
+nbformat.write(nb, 'test.ipynb')
+
